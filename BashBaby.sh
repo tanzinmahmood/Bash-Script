5bd048e70151bd271ce8427237067bad7ca891b7
tanzinmahmood <mahmood.tanzin@gmail.com>
2022-12-22 16:22:49 -0800

    NewBashScript

---
+++ BashBaby.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#### This is a new bash ####
+adduser Hey Man
+adduser Ney Man
+##### Creating a file\Folder #####
+mkdir -p /home/tanzin/NewBashTest
+touch /home/tanzin/NewBashTest/Tasty.txt
+##### Assigning Permission #####
+chmod 775 /home/tanzin/NewBashTest/Tasty.txt /home/tanzin/NewBashTest
